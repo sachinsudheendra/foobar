294d988f346af4ee44bd3338d5ff4459df4b090a
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2024-02-26 20:03:37 +0530

    Adding two services

---
+++ kyc-service/src/main/java/com/test/foobar/kycService/KYCServiceApplication.java
@@ -0,0 +1,11 @@
+package com.test.foobar.kycService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KYCServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KYCServiceApplication.class, args);
+    }
+}
