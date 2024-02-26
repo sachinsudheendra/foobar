3fcbe2d5456abf052d2719bb8d5023cdf0f3b42b
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2024-02-26 23:01:52 +0530

    Adding sleep

--- kyc-service/src/main/java/com/test/foobar/kycService/KYCController.java
+++ kyc-service/src/main/java/com/test/foobar/kycService/KYCController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class KYCController {
     @PostMapping(value = "/processKYC", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> processKYC(@RequestBody Map<String, String> payload) throws IOException {
+    public ResponseEntity<?> processKYC(@RequestBody Map<String, String> payload) throws Exception {
+        Thread.sleep(200);
         if (payload.containsKey("mobileNumber")) {
             String mobileNumber = payload.get("mobileNumber");
             if (mobileNumber.startsWith("9")) {
